b1f6d56080230d7b60559e020968c13d8e84b585
Kazufumi Miura <miura_k@defactostandard.co.jp>
2019-04-03 10:35:05 +0900

    a

--- centos7.sh
+++ centos7.sh
@@ -19,4 +19,38 @@ yum install -y httpd
 # install mysql
 
 # install samba
+yum install -y samba samba-client samba-common
 
+mv /etc/samba/smb.conf /etc/samba/smb.conf.bk
+touch /etc/samba/smb.conf
+
+cat <<EOF > /etc/samba/smb.conf
+[global]
+workgroup = MYGROUP
+server string = Samba Server Version %v
+netbios name = MYSERVER
+log file = /var/log/samba/log.%m
+max log size = 50
+security = user
+passdb backend = tdbsam
+load printers = yes
+cups options = raw
+
+[homes]
+comment = Home Directories
+browseable = no
+writable = yes
+
+path = /
+writable = yes
+guest ok = yes
+guest only = yes
+create mode = 0777
+directory mode = 0777
+share modes = yes
+EOF
+
+systemctl enable smb.service
+systemctl enable nmb.service
+systemctl restart smb.service
+systemctl restart nmb.service
