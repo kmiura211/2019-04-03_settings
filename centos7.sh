015c3a0f84933cb03ca0dc3e2061406f493f0f2b
Kazufumi Miura <miura_k@defactostandard.co.jp>
2019-04-03 10:18:40 +0900

    a

--- centos7.sh
+++ centos7.sh
@@ -9,3 +9,14 @@ sed s/enforcing/disabled/ /etc/selinux/config
 # disable firewall
 systemctl stop firewalld
 systemctl disable firewalld
+
+# install apache
+yum install -y httpd
+
+# install php
+
+
+# install mysql
+
+# install samba
+
